3737540b1112f3625bf8f7a065676a0ec3612d81
Tanya Fomina <fomina.tatianaaa@yandex.ru>
2021-11-22 20:37:51 +0200

    Update server

--- server.js
+++ server.js
@@ -2,10 +2,16 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
+const history = require('connect-history-api-fallback')
 
 const app = express()
-
-app.use(serveStatic(path.join(__dirname, 'dist')))
 const port = process.env.PORT || 80
 
+app.use(history({
+  verbose: true
+}))
+app.use(express.static('dist'));
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
 app.listen(port)
